46b961ecf5a95e1f9662da96db62b4148d2fa98b
Vitali Br <epamc@mail.ru>
2018-02-21 03:41:18 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = [] ) {
+	    var n = 0;
+	for (var i = 0; i < preferences.length; i++) {
+		var x = preferences[i];
+		var y = preferences[x-1];
+		var z = preferences[y-1];
+		if ( i==z-1 ) n++;
+	};
+	return Math.floor(n/3);
   // your implementation
 };
